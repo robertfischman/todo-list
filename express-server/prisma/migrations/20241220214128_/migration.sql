a19622cafef55b9c9ec66b4c1cd28d33af44fa6a
Robert Alan Fischman <robertalan0706@gmail.com>
2025-01-19 15:47:50 -0600

    final change

--- express-server/prisma/migrations/20241220214128_/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "color" INTEGER NOT NULL,
-    "completed" BOOLEAN NOT NULL DEFAULT false
-);
